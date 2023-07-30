c9593b060856779c3d546f4b13eeafd7c4d6e254
ImFstAsFckBoi <m.olivesten@protonmail.com>
2023-07-30 11:42:28 +0200

    Added deadlock detection at exit

--- pKernel/pKernel.c
+++ pKernel/pKernel.c
@@ -1,5 +1,6 @@
 #include "pKernel.h"
 #include "debug.h"
+#include "context.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -25,17 +26,18 @@ pcb_t *pk_add_proc(proc_func_t entry_func, char *name, arg_t args)
     // Load argv data into top of stack
     for (int i = 0; i < args.argc; ++i)
     {
+        // Copy each arg to process' own stack
         char *arg = args.argv[i];
         char *ptr = pk_stack_cpy(p, arg, strlen(arg) + 1);
+        // Reuse args.argv to remember new address for each arg
         args.argv[i] = ptr;
     }
     char **argv = pk_stack_cpy(p, args.argv, args.argc * sizeof(char *));
 
-  
-
     // Load return address and argc and argv
 
     // Move frame to not include argv data
+    // argc and argv list is part of frame
     p->cpu_state.BP.b32 = p->cpu_state.SP.b32;
 
     pk_stack_pushl(p, (DWORD) argv);
@@ -113,7 +115,7 @@ void pk_run(void)
 pcb_t *pk_default_next_proc(void)
 {
     if (kernel.current == &kernel.main_proc)
-        return (pcb_t *) list_at(&kernel.plist, 0);
+        return (pcb_t *) list_pop_front(&kernel.plist);
 
     size_t offset = list_find(&kernel.plist, (DWORD) kernel.current);
 
@@ -142,9 +144,35 @@ pcb_t *pk_default_next_proc(void)
 
 void pk_cleanup(void)
 {
+    pk_list list;
+    list_init(&list);
+
     for (size_t i = 0; i < kernel.plist.size; ++i)
     {
-        register pcb_t *p = (pcb_t *) list_at(&kernel.plist, i);
+        register pcb_t *p = ((pcb_t *)list_at(&kernel.plist, i));
+
+        if (p->status != DONE)
+        {
+            list_push_back(&list, (DWORD)p->name);
+        }
+    }
+
+    printf("Kernel exited with process");
+    for (size_t i = 0; i < list.size-1; ++i)
+    {
+        printf(" \"%s\"", (char *)list_at(&list, i));
+    }
+    printf(" and \"%s\" unfinished.\n", (char *)list_at(&list, list.size-1));
+
+    if (list.size > 1)
+    {
+        printf("This is likely caused to a deadlock.\n");
+    }
+
+    for (size_t i = 0; i < kernel.plist.size; ++i)
+    {
+        register pcb_t *p = ((pcb_t *)list_at(&kernel.plist, i));
+
         sema_destroy(&p->done_signal);
         list_destroy(&p->subprocesses);
         free(p->stack - STACK_SIZE);
